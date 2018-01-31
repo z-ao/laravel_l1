2bbd98d79296b568487ff76f08925a783a4c0718
Lea <452224689@qq.com>
2018-01-31 17:53:10 +0800

    finsh layout and routes

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,8 +2,15 @@
 <html>
 <head>
 	<title>@yield('title', 'Sample') - Laravel 入门教程</title>
+	<link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
-	@yield('content')
+	@include('layouts._header')
+
+    <div class="container">
+		@yield('content')
+
+		@include('layouts._footer')
+    </div>
 </body>
 </html>

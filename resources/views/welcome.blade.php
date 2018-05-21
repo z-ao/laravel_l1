483077456698df747062c0422df9bacfeb8beffe
Lea <452224689@qq.com>
2018-05-21 09:50:09 +0800

    v2_base-config

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel - by noname
                 </div>
 
                 <div class="links">

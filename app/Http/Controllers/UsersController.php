1c967054cc2bd9ffbc2447fe632a75f5ae744b25
Lea <452224689@qq.com>
2018-05-24 21:00:54 +0800

    v2 Finish login and logout

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Models\User;
 
+use Auth;
+
 class UsersController extends Controller
 {
     public function create()
@@ -32,6 +34,8 @@ class UsersController extends Controller
             'password' => bcrypt($request->password)
         ]);
 
+
+        Auth::login($user);
         session()->flash('success', '欢迎，您将在这里开启一段新的旅程～');
         return redirect()->route('users.show', [$user]);
     }

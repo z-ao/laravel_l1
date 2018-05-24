0dceb99dfb2d90cd5f2a5fc3de3aee6583193ae7
Lea <452224689@qq.com>
2018-05-24 22:02:06 +0800

    v2 Finish user edit, update, index, and destroy actions

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,5 @@ Route::resource('users', 'UsersController');
 Route::get('login', 'SessionsController@create')->name('login');
 Route::post('login', 'SessionsController@store')->name('login');
 Route::delete('logout', 'SessionsController@destroy')->name('logout');
+
+Route::get('/users/{user}/edit', 'UsersController@edit')->name('users.edit');

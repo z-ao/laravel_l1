e80c917071e323becb88127fbe54499a3f5a7473
Lea <452224689@qq.com>
2018-05-28 16:27:47 +0800

    v4 Finish layout and routes

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,8 +2,14 @@
 <html>
 <head>
     <title>@yield('title', 'Sample App') - Laravel 新手入门教程</title>
+    <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
-    @yield('content
+    @include('layouts._header')
+
+    <div class="container">
+        @yield('content')
+        @include('layouts._footer')
+    </div>
 </body>
 </html>
